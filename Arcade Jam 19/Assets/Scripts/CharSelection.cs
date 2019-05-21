899ddafbf7efc6636fcbdb351860cebf4a4cff5e
Maurice <40207470+Eelazar@users.noreply.github.com>
2019-05-21 16:07:40 +0200

    Added Menu Scene

---
+++ Arcade Jam 19/Assets/Scripts/CharSelection.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharSelection : MonoBehaviour
+{
+    public GameObject[] playerFields;
+    public GameObject[] hintFields;
+
+    private bool player1;
+    private bool player2;
+    private bool player3;
+    private bool player4;
+
+    void Start()
+    {
+        foreach(GameObject go in playerFields)
+        {
+            go.SetActive(false);
+        }
+
+        foreach (GameObject go in hintFields)
+        {
+            go.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && player1 == false)
+        {
+            hintFields[0].SetActive(false);
+            playerFields[0].SetActive(true);
+
+            player1 = true;
+        }
+        if (Input.GetKeyDown(KeyCode.B) && player2 == false)
+        {
+            hintFields[1].SetActive(false);
+            playerFields[1].SetActive(true);
+
+            player2 = true;
+        }
+        if (Input.GetKeyDown(KeyCode.C) && player3 == false)
+        {
+            hintFields[2].SetActive(false);
+            playerFields[2].SetActive(true);
+
+            player3 = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D) && player4 == false)
+        {
+            hintFields[3].SetActive(false);
+            playerFields[3].SetActive(true);
+
+            player4 = true;
+        }
+    }
+}
