4344316c8bdd46c4013007d60d5d9b67f6ac0aab
Maurice <40207470+Eelazar@users.noreply.github.com>
2019-05-23 17:00:25 +0200

    Menu Changes

--- Arcade Jam 19/Assets/Scripts/CharSelection.cs
+++ Arcade Jam 19/Assets/Scripts/CharSelection.cs
@@ -35,6 +35,14 @@ public class CharSelection : MonoBehaviour
 
             Static.player1 = true;
         }
+        else if (Input.GetKeyDown(KeyCode.A) && Static.player1 == true)
+        {
+            hintFields[0].SetActive(true);
+            playerFields[0].SetActive(false);
+
+            Static.player1 = false;
+        }
+
         if (Input.GetKeyDown(KeyCode.B) && Static.player2 == false)
         {
             hintFields[1].SetActive(false);
@@ -42,6 +50,14 @@ public class CharSelection : MonoBehaviour
 
             Static.player2 = true;
         }
+        else if (Input.GetKeyDown(KeyCode.B) && Static.player2 == true)
+        {
+            hintFields[1].SetActive(true);
+            playerFields[1].SetActive(false);
+
+            Static.player2 = false;
+        }
+
         if (Input.GetKeyDown(KeyCode.C) && Static.player3 == false)
         {
             hintFields[2].SetActive(false);
@@ -49,12 +65,27 @@ public class CharSelection : MonoBehaviour
 
             Static.player3 = true;
         }
+        else if (Input.GetKeyDown(KeyCode.C) && Static.player3 == true)
+        {
+            hintFields[2].SetActive(true);
+            playerFields[2].SetActive(false);
+
+            Static.player3 = false;
+        }
+
         if (Input.GetKeyDown(KeyCode.D) && Static.player4 == false)
         {
             hintFields[3].SetActive(false);
             playerFields[3].SetActive(true);
 
             Static.player4 = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && Static.player4 == true)
+        {
+            hintFields[3].SetActive(true);
+            playerFields[3].SetActive(false);
+
+            Static.player4 = false;
         }
     }
 }
